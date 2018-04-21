01de9782372e1ee9815852048cab210365ebb3db
akokuro <akokuro@mail.ru>
2018-04-21 14:11:14 +0400

    first commit

---
+++ src/neo/Wind.java
@@ -0,0 +1,90 @@
+package neo;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.GridLayout;
+import javax.swing.JTextArea;
+import java.awt.event.InputMethodListener;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+import java.awt.event.InputMethodEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class Wind {
+	JTextArea BblBod;
+	JTextArea Vvod;
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Wind window = new Wind();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Wind() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 648, 390);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new GridLayout(1, 0, 0, 0));
+
+		Vvod = new JTextArea();
+		Vvod.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent arg0) {
+				key_pressed();
+			}
+		});
+		frame.getContentPane().add(Vvod);
+
+		BblBod = new JTextArea();
+		frame.getContentPane().add(BblBod);
+
+	}
+
+	private void key_pressed() {
+		System.out.println("колбек работат");
+		String s = Vvod.getText();
+		Matcher m = Pattern.compile("[а-я]* ").matcher(s);
+		int startPoz, endPoz;
+		startPoz = endPoz = Vvod.getCaretPosition();
+		while (startPoz >= 0 && startPoz < s.length()
+				&& (s.toCharArray()[startPoz] != ' ' && s.toCharArray()[startPoz] != '\n'))
+			startPoz--;
+		while (
+				endPoz >= 0 && 
+				endPoz < s.length() && 
+				(s.toCharArray()[endPoz] != ' ' && s.toCharArray()[endPoz] != '\n'))
+			endPoz++;
+		try {
+			if (startPoz != endPoz)	
+				BblBod.setText(ne.Pars(s.substring(startPoz, endPoz)));
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+	}
+}
---
+++ src/neo/ne.java
@@ -0,0 +1,117 @@
+package neo;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class ne {
+	public static URL page = null;
+	public static String result = "";
+	public static String patt = "<li>";
+	public static int last_ind = 0;
+	public static String str = "ничего нет";
+	public static String word = "";
+	public static String l;
+	public static String query;
+
+	public static void main(String s) throws IOException {
+		ReadWord();
+		Pars(l);
+		Safe();
+	}
+
+	public static void ReadWord() {
+		Scanner b = new Scanner(System.in);
+		l = b.nextLine();
+		try {
+			query = URLEncoder.encode(l, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			System.out.println(
+					"Упа~ло при попытке закодировать строку в кусок УРЛ, эх - беда, печаль, можете попробовать снова");
+			e.printStackTrace();
+		}
+		b.close();
+	}
+
+	public static String Pars(String query) {
+		try {
+			System.out.println(query);
+			query = URLEncoder.encode(query, "UTF-8");
+		} catch (Exception e) {
+			System.out.println(
+					"Упа~ло при попытке закодировать строку в кусок УРЛ, эх - беда, печаль, можете попробовать снова");
+			e.printStackTrace();
+		}
+		System.out.println("ЭЭ");
+		
+		try {
+			Path file = Paths.get("C:\\Games\\Magic.txt");
+			page = new URL("http://rifma-online.ru/rifma/" + query + "/");
+			try {
+				InputStream in = page.openStream();
+				Files.copy(in, file, StandardCopyOption.REPLACE_EXISTING);
+			} catch (RuntimeException e) {
+				System.out.println("С этим вашим интернетом беда полная");
+			}
+			
+			Scanner a = new Scanner(page.openStream());
+			System.out.println(a);
+			while (a.hasNext()) {
+				str = a.nextLine();
+				for (int i = 0; i < str.length(); i++) {
+					last_ind = str.indexOf(patt, last_ind);
+					if (last_ind == -1) {
+						break;
+					} else {
+						last_ind += 20;
+						while (str.charAt(last_ind) != '>') {
+							last_ind++;
+						}
+						last_ind++;
+						while (str.charAt(last_ind) != '<') {
+							word += str.charAt(last_ind);
+							last_ind++;
+						}
+						word = new String(word.getBytes(), "UTF-8");
+						System.out.println(word);
+						result += word;
+						result = result + "\r\n";
+						word = "";
+					}
+				}
+			}
+			Safe();
+			return result;
+		} catch (Exception e) {
+			return ("Упало!!!! Парс сломался!!!! Караул!!!! Кто спасет нас от беды?!!!");
+		}
+
+	}
+
+	public static void Safe() {
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter("C:\\Games\\word.txt"))) {
+			bw.write(result);
+		} catch (IOException ex) {
+			System.out.println(
+					"Упало при попытке сохранить в файл - еще большая печаль, эх. Труды все напрасны. Как тщетен мир.");
+			System.out.println(ex.getMessage());
+		}
+	}
+}
